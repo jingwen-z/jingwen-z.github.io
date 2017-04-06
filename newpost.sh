7a7ab85da00e11bbfde0ba375198ded0ebc62b34
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-04-06 22:58:16 +0200

    script written by mincong, created for new post

---
+++ newpost.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#    This script creates a new blog post with metadata in ./_posts
+#    folder. Date will be generated according to the current time in
+#    the system. Usage:
+#
+#        ./newpost.sh "My Blog Post Title"
+#
+
+title=$1
+
+if [[ -z "$title" ]]; then
+    echo "usage: newpost.sh \"My New Blog\""
+    exit 1
+fi
+
+bloghome=$(cd "$(dirname "$0")" || exit; pwd)
+url=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+filename="$(date +"%Y-%m-%d")-$url.md"
+filepath="$bloghome/_posts/$filename"
+
+if [[ -f "$filepath" ]]; then
+    echo "$filepath already exists."
+    exit 1
+fi
+
+cat << EOF >> "$filepath"
+---
+layout:      post
+title:       "$title"
+date:        "$(date +"%Y-%m-%d %H:%M:%S %z")"
+categories:  [weekly]
+comments:    true
+---
+EOF
+
+echo "Blog created: $filepath"
